dc2ef27fbbf276b3495140d49abb1905a13d717d
Eko Nurwahyudi <generasic@gmail.com>
2020-04-16 12:14:45 +0700

    belajar vuex

---
+++ vuex/Hello.js
@@ -0,0 +1,14 @@
+export const Hello = {
+    template:`
+        <p>
+            State counter pada hello :
+            {{ counter }}
+        </p>
+    `,
+
+    computed: {
+        counter(){
+            return StorageEvent.state.counter
+        }
+    }
+}
